5f1d83eee1799e19358fe162b697c81da6849d7e
Jonathan Haines <jonno.haines@gmail.com>
2015-11-24 19:57:50 +1100

    Semi responsive footer

--- src/js/components/Root.jsx
+++ src/js/components/Root.jsx
@@ -7,6 +7,10 @@ export class Root extends React.Component {
 		render() {
 			return (<html>
 						<head>
+								<meta charset="utf-8"></meta>
+								<meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+								<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+								<title>Document</title>
 								<title>{this.props.title}</title>
 								<link rel="stylesheet" type="text/css" href="main.css"></link>
 								<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"></link>
