8cb4af5b439004f48f1b89af5477bfc906de5cd7
readeral <areader0@gmail.com>
2015-12-04 17:58:00 +1100

    updating hero elements, removing search box placeholder

--- src/js/components/Hero.jsx
+++ src/js/components/Hero.jsx
@@ -4,13 +4,16 @@ export class Hero extends React.Component {
 
 	render() {
 		return (
-			<div className = "hero-background">
+			<div className="hero-container">
+			<div className="hero-background"></div>
+			<div className="hero-content">
 				<div className="tagline">
 					<h1>Can a church really do IT <em>effectively?</em><br />
 					<em>Anything</em> is possible.<br />
 					Come on, we'll show you.</h1>
 				</div>
 			</div>
+		</div>
 		);
 	}
 }
--- src/js/components/MainMenu.jsx
+++ src/js/components/MainMenu.jsx
@@ -31,21 +31,6 @@ export class MainMenu extends React.Component {
 				<ul>
 					<li className="search-box text-uppercase">
 						<h3><a href="#"><i className="fa fa-search fa-lg"></i><div hidden>Search</div></a></h3>
-						<div className="search-menu">
-							<div className="search-field">
-								<form>
-									<input type="search" name="v100itsearch" placeholder="Search..." />
-									<input type="submit" />
-								</form>
-							</div>
-							<ul>
-								<li><h3>Help + How To</h3></li>
-								<li><h3>Support Request</h3></li>
-								<li><h3>Feature Request</h3></li>
-								<li><h3>System Status</h3></li>
-								<li><h3>About Us</h3></li>
-							</ul>
-						</div>
 					</li>
 
 						{this.state.links.map((link, key) =>
