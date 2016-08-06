13e062226074132b9f963c50f640641a00795805
readeral <areader0@gmail.com>
2016-08-06 19:41:12 +1000

    Broken stuff

--- src/routes/Consultation.jsx
+++ src/routes/Consultation.jsx
@@ -1,64 +1,16 @@
 import React from 'react';
 import {Index} from '../components/Index/index.jsx';
+import {Markdown} from '../components/Markdown';
+import content from '../content/consultation.md';
 
 export const Consultation = () => (
 	<Index>
 		<div className="consultation-wrapper">
 			<div className="consultation-overlay">
 				<div className="site-wrapper site-wrapper-padding">
-					<h1>Consultation</h1>
-					<section>
-						<p>The purpose of Vision 100 IT Training and Consultancy is to provide tailored help to ministry leaders and/or ministry admins using IT in the service of the mission of their ministry. We long to help ministries become efficient and effective by doing things in the clearest, easiest and fastest way possible.</p>
-						<h3>Who is Vision 100 IT for?</h3>
-						<ul>
-							<li>Vision 100 IT is part of The Vision 100 Network, a Reformed evangelical church planting network in Tasmania.</li>
-							<li>We serve churches from a range of networks: Presbyterian, Christian Reformed Church, Baptist, FIEC, Anglican.</li>
-							<li>We require agreement to our simple doctrinal basis from our clients: 'http://vision100.org/beliefs'</li>
-							<li>Our main area of focus is small and medium sized churches (under 400 people). However we have knowledge and skills to assist larger churches too.</li>
-						</ul>
-
-						<h2>Consultation and training framework</h2>
-						<p>For some churches, it can simply be more efficient and effective to have someone on the ground with you as you transition through IT changes. Meeting with your key staff, stakeholders and your local IT overseer (from within your church) can make sure that you hit the ground running.</p>
-
-						<p>Vision 100 IT offers on-site consultancy anywhere in the country, with the following time commitments:</p>
-						<ul>
-							<li>Initial meeting to clarify needs and expectations (1 hour)</li>
-							<li>3 day on-site visit</li>
-							<li>Post-visit report</li>
-							<li>3 post-visit coaching meetings (1 hour each)</li>
-						</ul>
-						<p>For some churches, onsite is not possible, or is cost prohibitive - but you'd still like your staff to meet virtually with Vision 100 IT to ensure that the new tools and expectations are clearly understood.</p>
-						<p>Vision 100 IT can offer virtual consultancy anywhere in the country, with the following time commitments:</p>
-						<ul>
-							<li>Initial meeting to clarify needs and expectations (1 hour)</li>
-							<li>6 coaching sessions (1 hour each)</li>
-							<li>For smaller churches, smaller parachurch ministries (under 50 adults average attendance) and for church plants we can negotiate a lower consultancy fee.</li>
-						</ul>
-
-						<h3> Our consultancy process</h3>
-						<p>In our initial conversation, we seek to discover the context, history, felt needs and existing strategies of the ministry.</p>
-						<p>After this initial conversation we cover the following areas, with a focus on those which are most significant:</p>
-						<ul>
-							<li><strong>Information flow:</strong> the way that information is gathered, updated, shared and erased within the ministry. This also includes attention to conformity with the Australian Privacy Principles and any denominational privacy policies.</li>
-							<li><strong>Alignment:</strong> the way IT is being used works with rather than against your philosophy of ministry, strategy and ministry structures.</li>
-							<li><strong>Integration:</strong> all your IT and other tools working together in the most efficient and effective way. Removing any unnecessary steps, improving automation.</li>
-							<li><strong>Training and management processes:</strong> how volunteers and staff are equipped to use IT tools and helped over time to continue to use them well.</li>
-							<li><strong>Feature maximisation:</strong> using IT solutions to get the best possible advantages from them. Discovering features that are not yet being used to their full capacity, using features correctly, avoiding unnecessary features.</li>
-							<li><strong>Communications plan:</strong> how is vision and practical information communicated clearly and consistently to staff, leaders, volunteers, members and outsiders?</li>
-							<li><strong>Updating and assessing:</strong> setting to keep new patterns working well, and planning to regularly assess and improve.</li>
-						</ul>
-						<h3>Community tools and events</h3>
-						<p>A vital supplement to tailored consultancy is the broader collection of tools and events that support ministries in their efforts to use IT for the cause of the gospel.</p>
-						<p>We have a range of documentation, blog posts and other social media input to keep ministries growing and developing.</p>
-						<p>We also run training events, podcasts and webinars to provide input on general areas of need. </p>
-						<p>Our community tools also put ministries in touch with others who are also working at similar challenges, so that they can learn from one another.</p>
-
-						<h2>Pricing</h2>
-						<p>Our on-site consultancy carries a cost of $2500. This is all-inclusive cost, covering all travel, accommodation and food for the duration of the agreed consultation period.</p>
-						<p>Extra travel surplus would be required for Darwin, Perth, north of Brisbane, regional areas and outside Australia. This will be priced on enquiry.</p>
-						<p>Our virtual consultancy carries a cost of $750 for the duration of the agreed consultancy.</p>
-						<p>For smaller churches, smaller parachurch ministries (under 50 adults average attendance) and for church plants we can negotiate a lower consultancy fee.</p>
-					</section>
+					<Markdown>
+						{content}
+					</Markdown>
 				</div>
 			</div>
 		</div>
--- src/routes/Podcasting.jsx
+++ src/routes/Podcasting.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import {Index} from '../components/Index/index.jsx';
+import {Markdown} from '../components/Markdown';
 
 export const Podcasting = () => (
 	<Index>
 		<div className="podcasting-wrapper">
 			<div className="podcasting-overlay">
 				<div className="site-wrapper site-wrapper-padding">
-					<h1>Podcasting Module</h1>
-					<section>
-						<p>Podcasting is an excellent way to distribute your church audio, from your weekly sermons, to your once off evangelistic events and community seminars. Podcasting allows your church members and others to *automatically* download your audio to their devices.</p>
 
-						<h3>Why podcasting is great</h3>
-						<p>The main issue churches have with podcasting is the significant effort it takes to set it up. Often it's enough trouble to post the file on the website. Unfortunately, this then requires church members to regularly return to the website to download the individual files, and if they then want to listen on a digital device like a phone or tablet, it needs to be added to their library, and synced before it can be listened to!</p>
-						<p>Needless to say - the effort of uploading sermons has a limited impact when church members find it difficult to listen to them.</p>
-						<p>This is why Vision 100 IT offers podcasting as a core feature of our websites. Once set up, it's as simple as uploading the file to your website and the rest is handled automatically.</p>
-						<p>Podcasting can be linked to the iTunes podcasting system as well, allowing you to use your church branding and to have your church content discoverable on iTunes.</p>
-						<p>If you're interested in having podcasting added to your website, let us know through the feature request form. If you currently don't have a Vision 100 IT hosted website, but are interested in podcasting on your own page, we'd be happy to let you know what you need to consider in order to set it up for yourself.</p>
-					</section>
 				</div>
 			</div>
 		</div>
