6eb6c23538fd6bc0a6f3abccceb5e0fb1fd4025a
readeral <areader0@gmail.com>
2016-11-26 22:29:04 +1100

    changing to :local, and fixing search results partially

--- src/components/search-results/index.jsx
+++ src/components/search-results/index.jsx
@@ -1,7 +1,10 @@
 import React, {PropTypes} from 'react';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 import {Link} from 'react-router';
+import FirstChild from '../../lib/first-child';
 import styles from './SearchResults.scss';
+import height from './heightTransition.scss';
+import opacity from './opacityTransition.scss';
 
 const SearchResults = ({onCloseResult, onCloseModal, searchResults, titleClassName}) => {
 	const results = searchResults.map((item, key) => (
@@ -11,29 +14,35 @@ const SearchResults = ({onCloseResult, onCloseModal, searchResults, titleClassNa
 	));
 
 	return (
-		<div className={styles.results}>
-			<div className={titleClassName}>
-				<h3>
-					Results
-					<a onClick={onCloseResult}>
-						<span className="fa fa-times-circle"/>
-					</a>
-				</h3>
-			</div>
-			<div className={styles.content}>
-				<CSSTransitionGroup
-					component="ul"
-					transitionName={styles}
-					transitionEnterTimeout={300}
-					transitionLeaveTimeout={300}
-					>
-					{results}
-				</CSSTransitionGroup>
-			</div>
-			<div className={`small hidden ${styles.nav}`}>
-				<p><Link to="/documentation?search=poop">more</Link></p>
-			</div>
-		</div>
+		<CSSTransitionGroup
+			component={FirstChild}
+			transitionName={height}
+			transitionEnterTimeout={500}
+			transitionLeaveTimeout={500}
+			>
+			{searchResults.length &&
+				<div>
+					<div className={titleClassName}>
+						<h3> Results <a onClick={onCloseResult}> <span className="fa fa-times-circle"/> </a> </h3>
+					</div>
+					<div className={styles.content}>
+						<CSSTransitionGroup
+							component="ul"
+							transitionName={opacity}
+							transitionEnterTimeout={300}
+							transitionLeaveTimeout={300}
+							>
+							{results}
+						</CSSTransitionGroup>
+					</div>
+					<div className={`small hidden ${styles.nav}`}>
+						<p>
+							<Link to="/documentation?search=poop">more</Link>
+						</p>
+					</div>
+				</div>
+			}
+		</CSSTransitionGroup>
 	);
 };
 
