7de057ea39e614e55041c2cd2d594b38a0601d91
Bratalix <bratalix@gmail.com>
2018-04-13 10:46:47 +1000

    Syntax fixes

--- src/components/featured/feature-pricing.js
+++ src/components/featured/feature-pricing.js
@@ -62,7 +62,7 @@ const FeaturedPricing = () => (
 				</tr>
 			</table>
 
-			<p>We have lower annual fees for parachurch ministries, click on 'More about costs' below for more information.</p>
+			<p>We have lower annual fees for parachurch ministries, click on &#39;More about costs&#39; below for more information.</p>
 
 			<p className="lead">
 				<em>Consultation services</em>
