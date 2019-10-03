b9f7bae1bbe598db6b4825d9962c57219ea719c8
Alan Reader <areader0@gmail.com>
2019-10-03 14:31:43 +1000

    minor changes to ensure up to date

--- src/containers/layout/index.js
+++ src/containers/layout/index.js
@@ -12,19 +12,19 @@ const Layout = () => (
 				<Switch>
 					<Route exact path="/" component={Routes.Home}/>
 					<Route exact path="/about" component={Routes.About}/>
-					<Route exact path="/blog/:blogId" component={Routes.Blog}/>
-					<Route exact path="/client/:page?" component={Routes.Client}/>
+					{/*<Route exact path="/blog/:blogId" component={Routes.Blog}/>*/}
+					{/*<Route exact path="/client/:page?" component={Routes.Client}/>*/}
 					<Route exact path="/consultation" component={Routes.Consultation}/>
 					<Route exact path="/contact" component={Routes.Contact}/>
-					<Route exact path="/control" component={Routes.Control}/>
+					{/*<Route exact path="/control" component={Routes.Control}/>*/}
 					<Route exact path="/documentation/:documentId" component={Routes.DocumentationTemplate}/>
-					<Route exact path="/documentation" component={Routes.Documentation}/>
+					{/*<Route exact path="/documentation" component={Routes.Documentation}/>*/}
 					<Route exact path="/elvanto" component={Routes.Elvanto}/>
 					<Route exact path="/feature" component={Routes.Feature}/>
 					<Route exact path="/podcasting" component={Routes.Podcasting}/>
 					<Route exact path="/registration" component={Routes.Registration}/>
-					<Route exact path="/sparkleshare" component={Routes.Sparkleshare}/>
-					<Route exact path="/status" component={Routes.Status}/>
+					{/*<Route exact path="/sparkleshare" component={Routes.Sparkleshare}/>*/}
+					{/*<Route exact path="/status" component={Routes.Status}/>*/}
 					<Route exact path="/support" component={Routes.Support}/>
 					<Route exact path="/training" component={Routes.Training}/>
 					<Route component={Routes.Error}/>
